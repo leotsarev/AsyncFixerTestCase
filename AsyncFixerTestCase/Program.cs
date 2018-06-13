4f514682bb4bc2901cc6b83d6afcdb1678ae2448
Leonid Tsarev <leo@bastilia.ru>
2018-06-13 11:40:35 +0300

    Initial version

--- AsyncFixerTestCase/Program.cs
+++ AsyncFixerTestCase/Program.cs
@@ -1,15 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace AsyncFixerTestCase
 {
     class Program
     {
-        static void Main(string[] args)
+        private static Task Main(string[] args)
         {
+            return AwaitSkipped();
+        }
+
+        private static Task AwaitSkipped()
+        {
+            ReturnVoid(); //Expect some kind of warning
+            return Task.CompletedTask;
+        }
+
+        private static async Task AwaitSkippedInAsync()
+        {
+            await ReturnVoid();
+            ReturnVoid(); // CS4014
+        }
+
+        public static Task ReturnVoid()
+        {
+            //Expect AsyncFixer05
+            return Task.FromResult(true);
+        }
+
+        public static Task<int> ReturnInt()
+        {
+            return Task.FromResult(1);
+        }
+
+        public static Task ConvertIntToVoid()
+        {
+            //Expect AsyncFixer05
+            var convertIntToVoid = (Task) ReturnInt();
+            return convertIntToVoid;
         }
     }
 }
